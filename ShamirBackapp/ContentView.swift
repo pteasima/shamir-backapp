c4a55b12b361a27d65aec36f728904f0144a7a4c
Petr Sima <ptea.sima@gmail.com>
2020-04-08 12:42:12 +0200

    Initial Commit

---
+++ ShamirBackapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShamirBackapp
+//
+//  Created by Petr Šíma on 08/04/2020.
+//  Copyright © 2020 Petr Šíma. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
