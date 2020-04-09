6c9fa62d47a7c84359c90697bda86a8443515720
Petr Sima <ptea.sima@gmail.com>
2020-04-09 16:52:23 +0200

    small refactor

--- ShamirKit/Sources/ShamirKit/ShamirKit.swift
+++ ShamirKit/Sources/ShamirKit/ShamirKit.swift
@@ -1,10 +1,12 @@
 import Foundation
 
-func shamirWithNumbers<G: RandomNumberGenerator>(_ input: UInt64, using rng: inout G) -> [UInt64] {
+func shamir<G: RandomNumberGenerator>(secret: UInt64, using rng: inout G) -> [UInt64] {
   let grad = UInt64.random(in: UInt64.min...10000, using: &rng)
-  return [input + grad, input + 2 * grad]
+  return [secret + grad, secret + 2 * grad]
 }
 
-func shamir(_ input: String, rngSeed: Int = 0, numberOfShares: Int = 2) ->  [String] {
-  fatalError()
+func unshamir(shares: [UInt64]) -> UInt64 {
+  print(shares[0])
+  print(shares[1])
+  return UInt64(-(Int(shares[1]) - (2 * Int(shares[0]))))
 }
--- ShamirKit/Tests/ShamirKitTests/ShamirKitTests.swift
+++ ShamirKit/Tests/ShamirKitTests/ShamirKitTests.swift
@@ -4,15 +4,10 @@ import XCTest
 final class ShamirKitTests: XCTestCase {
   func testShamir() {
     var rng = LCRNG(seed: 6)
-    let shares = shamirWithNumbers(500, using: &rng)
+    let shares = shamir(secret: 500, using: &rng)
     
-    func secretFromShamirShares(shares: [UInt64]) -> UInt64 {
-      print(shares[0])
-      print(shares[1])
-      return UInt64(-(Int(shares[1]) - (2 * Int(shares[0]))))
-    }
     
-    XCTAssertEqual(500, secretFromShamirShares(shares: shares))
+    XCTAssertEqual(500, unshamir(shares: shares))
   }
   
   //afaik this is just for linux, we can reenable it if need be
