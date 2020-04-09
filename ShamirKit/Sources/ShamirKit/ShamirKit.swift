9e98da3fae977ef1dc5b987330fe0eaa644ace12
Petr Sima <ptea.sima@gmail.com>
2020-04-09 17:26:20 +0200

    give up on large numbers for now, just support small ints

--- ShamirKit/Sources/ShamirKit/ShamirKit.swift
+++ ShamirKit/Sources/ShamirKit/ShamirKit.swift
@@ -1,12 +1,12 @@
 import Foundation
 
-func shamir<G: RandomNumberGenerator>(secret: UInt64, using rng: inout G) -> [UInt64] {
-  let grad = UInt64.random(in: UInt64.min...10000, using: &rng)
+func shamir<G: RandomNumberGenerator>(secret: Int, using rng: inout G) -> [Int] {
+  let maxGrad = 100_000
+  precondition(secret < maxGrad)
+  let grad = Int.random(in: Int.min...maxGrad, using: &rng)
   return [secret + grad, secret + 2 * grad]
 }
 
-func unshamir(shares: [UInt64]) -> UInt64 {
-  print(shares[0])
-  print(shares[1])
-  return UInt64(-(Int(shares[1]) - (2 * Int(shares[0]))))
+func unshamir(shares: [Int]) -> Int {
+  return -(shares[1] - (2 * shares[0]))
 }
